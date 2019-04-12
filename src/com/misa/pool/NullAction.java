625940cdb8c0cc6f90174b228aad0e9a7b040f1d
misaonline <misaonline@qq.com>
2019-04-12 14:44:44 +0800

    增加功能
    增加功能

---
+++ src/com/misa/pool/NullAction.java
@@ -0,0 +1,24 @@
+package com.misa.pool;
+
+public class NullAction extends BaseAction 
+{
+
+	@Override
+	public void installBusiness() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void installAction() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispatchListener() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
